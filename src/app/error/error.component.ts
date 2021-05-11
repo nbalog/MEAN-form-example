fd40445ea44df3958531e38d7f1ef0d66081a008
die Katze <nbdiekatze@hotmail.com>
2021-05-11 13:42:22 +0200

    readme

--- src/app/error/error.component.ts
+++ src/app/error/error.component.ts
@@ -7,25 +7,9 @@ import { MAT_DIALOG_DATA } from "@angular/material/dialog";
 @Component({
   templateUrl: "./error.component.html",
   selector: "app-error",
-  // styleUrls: ["./error.component.css"]
 })
 export class ErrorComponent {
-  // data: { message: string };
-  // private errorSub: Subscription;
+
   constructor(@Inject(MAT_DIALOG_DATA) public data: { message: string }) {}
-  // constructor(private errorService: ErrorService) {}
 
-  // ngOnInit() {
-  //   this.errorSub = this.errorService.getErrorListener().subscribe(message => {
-  //     this.data = { message: message };
-  //   });
-  // }
-
-  // onHandleError() {
-  //   this.errorService.handleError();
-  // }
-
-  // ngOnDestroy() {
-  //   this.errorSub.unsubscribe();
-  // }
 }
